d0a6bdea90e97ad1bd59ca08a4a742c0af6e1033
Raül de Arriba <rauldearriba@gmail.com>
2022-10-26 15:41:29 +0200

    nivel 1-3

--- src/components/dados/DadosUno.js
+++ src/components/dados/DadosUno.js
@@ -11,15 +11,23 @@ export const DadosUno = () => {
     aUno: '',
     aDos: '',
     aTres: '',
+    aCuatro: '',
+    aCinco: '',
     nUno: '',
     nDos: '',
     nTres: '',
+    nCuatro: '',
+    nCinco: '',
     rUno: '',
     rDos: '',
     rTres: '',
+    rCuatro: '',
+    rCinco: '',
     lUno: '',
     lDos: '',
     lTres: '',
+    lCuatro: '',
+    lCinco: '',
   });
 
 
@@ -27,15 +35,23 @@ export const DadosUno = () => {
     aUno,
     aDos,
     aTres,
+    aCuatro,
+    aCinco,
     nUno,
     nDos,
     nTres,
+    nCuatro,
+    nCinco,
     rUno,
     rDos,
     rTres,
+    rCuatro,
+    rCinco,
     lUno,
     lDos,
     lTres,
+    lCuatro,
+    lCinco,
    } = formRegisterValues;
 
    let resultado = 0;
@@ -43,98 +59,124 @@ export const DadosUno = () => {
 
   const handleRegister = ( e ) => {
     e.preventDefault();
-    aUno = Number(aUno)
-    aDos = Number(aDos)
-    aTres = Number(aTres)
-    nUno = Number(nUno)
-    nDos = Number(nDos)
-    nTres = Number(nTres)
-    rUno = Number(rUno)
-    rDos = Number(rDos)
-    rTres = Number(rTres)
-    lUno = Number(lUno)
-    lDos = Number(lDos)
-    lTres = Number(lTres)
+    resultado = 0;
 
     // AMARILLOS
-    if (aUno >= 0 && aUno <= 6) {
-      if (aUno === 1 || aUno === 6){
+    if (aUno && Number(aUno) >= 0 && Number(aUno) <= 6) {
+      if (Number(aUno) === 1 || Number(aUno) === 6){
         resultado = resultado + 0;
+      } 
+      if (Number(aUno) ===2 || Number(aUno) === 3 || Number(aUno) === 4 || Number(aUno) === 5){
+        resultado = resultado + 1;
       }
-      if (aUno >= 2 && aUno <= 5){
+    }
+    
+    if (aDos && Number(aDos) >= 0 && Number(aDos) <= 6) {
+      if (Number(aDos) === 1 || Number(aDos) === 6){
+        resultado = resultado + 0;
+      } 
+      if (Number(aDos) ===2 || Number(aDos) === 3 || Number(aDos) === 4 || Number(aDos) === 5){
         resultado = resultado + 1;
       }
     }
 
-    if (aDos >= 0 && aDos <= 6) {
-      if (aDos === 1 || aDos === 6){
+    if (aTres && Number(aTres) >= 0 && Number(aTres) <= 6) {
+      if (Number(aTres) === 1 || Number(aTres) === 6){
         resultado = resultado + 0;
-      }
-      if (aDos >= 2 && aDos <= 5){
+      } 
+      if (Number(aTres) ===2 || Number(aTres) === 3 || Number(aTres) === 4 || Number(aTres) === 5){
         resultado = resultado + 1;
       }
     }
 
-    if (aTres >= 0 && aTres <= 6) {
-      if (aTres === 1 || aTres === 6){
+    if (aCuatro && Number(aCuatro) >= 0 && Number(aCuatro) <= 6) {
+      if (Number(aCuatro) === 1 || Number(aCuatro) === 6){
         resultado = resultado + 0;
+      } 
+      if (Number(aCuatro) ===2 || Number(aCuatro) === 3 || Number(aCuatro) === 4 || Number(aCuatro) === 5){
+        resultado = resultado + 1;
       }
-      if (aTres >= 2 && aTres <= 5){
+    }
+
+    if (aCinco && Number(aCinco) >= 0 && Number(aCinco) <= 6) {
+      if (Number(aCinco) === 1 || Number(aCinco) === 6){
+        resultado = resultado + 0;
+      } 
+      if (Number(aCinco) ===2 || Number(aCinco) === 3 || Number(aCinco) === 4 || Number(aCinco) === 5){
         resultado = resultado + 1;
       }
     }
 
     // NARANJA
-    if (nUno >= 0 && nUno <= 6) {
-
-      if (nUno === 5 || nUno === 3){
+    if (nUno && Number(nUno) >= 0 && Number(nUno) <= 6) {
+      if (Number(nUno) === 5 || Number(nUno) === 3){
         resultado = resultado + 2;
-      } else {
+      } 
+      if (Number(nUno) ===1 || Number(nUno) === 2 || Number(nUno) === 4 || Number(nUno) === 6){
         resultado = resultado + 1;
       }
     }
 
-    if (nDos >= 0 && nDos <= 6) {
-      if (nDos === 5 || nDos === 3){
+    if (nDos && Number(nDos) >= 0 && Number(nDos) <= 6) {
+      if (Number(nDos) === 5 || Number(nDos) === 3){
         resultado = resultado + 2;
-      } else {
+      } 
+      if (Number(nDos) ===1 || Number(nDos) === 2 || Number(nDos) === 4 || Number(nDos) === 6){
+        resultado = resultado + 1;
+      }
+    }
+    
+    if (nTres && Number(nTres) >= 0 && Number(nTres) <= 6) {
+      if (Number(nTres) === 5 || Number(nTres) === 3){
+        resultado = resultado + 2;
+      } 
+      if (Number(nTres) ===1 || Number(nTres) === 2 || Number(nTres) === 4 || Number(nTres) === 6){
         resultado = resultado + 1;
       }
     }
 
-    if (nTres >= 0 && nTres <= 6) {
-      if ( nTres === 5 || nTres === 3 ){
-        resultado = resultado+2;
-      } else {
-        resultado = resultado+1;
+    if (nCuatro && Number(nCuatro) >= 0 && Number(nCuatro) <= 6) {
+      if (Number(nCuatro) === 5 || Number(nCuatro) === 3){
+        resultado = resultado + 2;
+      } 
+      if (Number(nCuatro) ===1 || Number(nCuatro) === 2 || Number(nCuatro) === 4 || Number(nCuatro) === 6){
+        resultado = resultado + 1;
+      }
+    }
+
+    if (nCinco && Number(nCinco) >= 0 && Number(nCinco) <= 6) {
+      if (Number(nCinco) === 5 || Number(nCinco) === 3){
+        resultado = resultado + 2;
+      } 
+      if (Number(nCinco) ===1 || Number(nCinco) === 2 || Number(nCinco) === 4 || Number(nCinco) === 6){
+        resultado = resultado + 1;
       }
     }
 
     // ROJO
-    if (rUno >= 0 && rUno <= 6) {
-
-      if (rUno === 1){
+    if (rUno && Number(rUno) >= 0 && Number(rUno) <= 6) {
+      if (Number(rUno) === 1){
         resultado = resultado + 0;
       } 
-      if (rUno === 4) {
+      if (Number(rUno) === 4) {
         resultado = resultado + 1;
       }
-      if (rUno === 2 || rUno === 3|| rUno === 6) {
+      if (Number(rUno) === 2 || Number(rUno) === 3|| Number(rUno) === 6) {
         resultado = resultado + 2;
       }
-      if (rUno === 5) {
+      if (Number(rUno) === 5) {
         resultado = resultado + 3;
       }
     }
 
-    if (rDos >= 0 && rDos <= 6) {
-      if (rDos === 1){
+    if (rDos && Number(rDos) >= 0 && Number(rDos) <= 6) {
+      if (Number(rDos) === 1){
         resultado = resultado + 0;
       } 
-      if (rDos === 4) {
+      if (Number(rDos) === 4) {
         resultado = resultado + 1;
       }
-      if (rDos === 2 || rDos === 3|| rDos === 6) {
+      if (Number(rDos) === 2 || Number(rDos) === 3|| Number(rDos) === 6) {
         resultado = resultado + 2;
       }
       if (rDos === 5) {
@@ -142,43 +184,93 @@ export const DadosUno = () => {
       }
     }
 
-    if (rTres >= 0 && rTres <= 6) {
-      if (rTres === 1){
+    if (rTres && Number(rTres) >= 0 && Number(rTres) <= 6) {
+      if (Number(rTres) === 1){
         resultado = resultado + 0;
       } 
-      if (rTres === 4) {
+      if (Number(rTres) === 4) {
         resultado = resultado + 1;
       }
-      if (rTres === 2 || rTres === 3|| rTres === 6) {
+      if (Number(rTres) === 2 || Number(rTres) === 3|| Number(rTres) === 6) {
         resultado = resultado + 2;
       }
-      if (rTres === 5) {
+      if (Number(rTres) === 5) {
+        resultado = resultado + 3;
+      }
+    }
+
+    if (rCuatro && Number(rCuatro) >= 0 && Number(rCuatro) <= 6) {
+      if (Number(rCuatro) === 1){
+        resultado = resultado + 0;
+      } 
+      if (Number(rCuatro) === 4) {
+        resultado = resultado + 1;
+      }
+      if (Number(rCuatro) === 2 || Number(rCuatro) === 3|| Number(rCuatro) === 6) {
+        resultado = resultado + 2;
+      }
+      if (Number(rCuatro) === 5) {
+        resultado = resultado + 3;
+      }
+    }
+
+    if (rCinco && Number(rCinco) >= 0 && Number(rCinco) <= 6) {
+      if (Number(rCinco) === 1){
+        resultado = resultado + 0;
+      } 
+      if (Number(rCinco) === 4) {
+        resultado = resultado + 1;
+      }
+      if (Number(rCinco) === 2 || Number(rCinco) === 3|| Number(rCinco) === 6) {
+        resultado = resultado + 2;
+      }
+      if (Number(rCinco) === 5) {
         resultado = resultado + 3;
       }
     }
 
     // LILA
-    if (lUno >= 0 && lUno <= 6) {
-
-      if (lUno === 2 || lUno === 6){
+    if (lUno && Number(lUno) >= 0 && Number(lUno) <= 6) {
+      if (Number(lUno) === 2 || Number(lUno) === 6){
         resultado = resultado + 3;
-      } else {
+      } 
+      if (Number(lUno) !== 2 || Number(lUno) !== 6){
         resultado = resultado + 2;
       }
     }
 
-    if (lDos >= 0 && lDos <= 6) {
-      if (lDos === 2 || lDos === 6){
+    if (lDos && Number(lDos) >= 0 && Number(lDos) <= 6) {
+      if (Number(lDos) === 2 || Number(lDos) === 6){
         resultado = resultado + 3;
-      } else {
+      } 
+      if (Number(lDos) !== 2 || Number(lDos) !== 6){
         resultado = resultado + 2;
       }
     }
 
-    if (lTres >= 0 && lTres <= 6) {
-      if (lTres === 2 || lTres === 6){
+    if (lTres && Number(lTres) >= 0 && Number(lTres) <= 6) {
+      if (Number(lTres) === 2 || Number(lTres) === 6){
         resultado = resultado + 3;
-      } else {
+      }
+      if (Number(lTres) !== 2 || Number(lTres) !== 6){
+        resultado = resultado + 2;
+      }
+    }
+
+    if (lCuatro && Number(lCuatro) >= 0 && Number(lCuatro) <= 6) {
+      if (Number(lCuatro) === 2 || Number(lCuatro) === 6){
+        resultado = resultado + 3;
+      }
+      if (Number(lCuatro) !== 2 || Number(lCuatro) !== 6){
+        resultado = resultado + 2;
+      }
+    }
+
+    if (lCinco && Number(lCinco) >= 0 && Number(lCinco) <= 6) {
+      if (Number(lCinco) === 2 || Number(lCinco) === 6){
+        resultado = resultado + 3;
+      }
+      if (Number(lCinco) !== 2 || Number(lCinco) !== 6){
         resultado = resultado + 2;
       }
     }
@@ -226,6 +318,26 @@ export const DadosUno = () => {
                               onChange={ handleRegisterInputChange }
                           />
                       </div>
+                      <div className="form-group col">
+                          <input
+                              type="number"
+                              className="form-control login__yellow_inputs"
+                              placeholder="Amarillo4"
+                              name="aCuatro"
+                              value={ aCuatro }
+                              onChange={ handleRegisterInputChange }
+                          />
+                      </div>
+                      <div className="form-group col">
+                          <input
+                              type="number"
+                              className="form-control login__yellow_inputs"
+                              placeholder="Amarillo5"
+                              name="aCinco"
+                              value={ aCinco }
+                              onChange={ handleRegisterInputChange }
+                          />
+                      </div>
                     </div>
 
                     <div className="form-row">
@@ -234,7 +346,7 @@ export const DadosUno = () => {
                         <input
                             type="number"
                             className="form-control login__yellow_inputs"
-                            placeholder="Naranja3"
+                            placeholder="Naranja1"
                             name="nUno"
                             value={ nUno }
                             onChange={ handleRegisterInputChange }
@@ -244,7 +356,7 @@ export const DadosUno = () => {
                           <input
                               type="number"
                               className="form-control login__yellow_inputs"
-                              placeholder="Naranja3"
+                              placeholder="Naranja1"
                               name="nDos"
                               value={ nDos }
                               onChange={ handleRegisterInputChange }
@@ -257,6 +369,26 @@ export const DadosUno = () => {
                               placeholder="Naranja3"
                               name="nTres"
                               value={ nTres }
+                              onChange={ handleRegisterInputChange }
+                          />
+                      </div>
+                      <div className="form-group col">
+                          <input
+                              type="number"
+                              className="form-control login__yellow_inputs"
+                              placeholder="Naranja4"
+                              name="nCuatro"
+                              value={ nCuatro }
+                              onChange={ handleRegisterInputChange }
+                          />
+                      </div>
+                      <div className="form-group col">
+                          <input
+                              type="number"
+                              className="form-control login__yellow_inputs"
+                              placeholder="Naranja5"
+                              name="nCinco"
+                              value={ nCinco }
                               onChange={ handleRegisterInputChange }
                           />
                       </div>
@@ -294,6 +426,26 @@ export const DadosUno = () => {
                               onChange={ handleRegisterInputChange }
                           />
                       </div>
+                      <div className="form-group col">
+                          <input
+                              type="number"
+                              className="form-control login__yellow_inputs"
+                              placeholder="Rojo4"
+                              name="rCuatro"
+                              value={ rCuatro }
+                              onChange={ handleRegisterInputChange }
+                          />
+                      </div>
+                      <div className="form-group col">
+                          <input
+                              type="number"
+                              className="form-control login__yellow_inputs"
+                              placeholder="Rojo5"
+                              name="rCinco"
+                              value={ rCinco }
+                              onChange={ handleRegisterInputChange }
+                          />
+                      </div>
                     </div>
 
                     <div className="form-row">
@@ -325,6 +477,26 @@ export const DadosUno = () => {
                               placeholder="Lila3"
                               name="lTres"
                               value={ lTres }
+                              onChange={ handleRegisterInputChange }
+                          />
+                      </div>
+                      <div className="form-group col">
+                          <input
+                              type="number"
+                              className="form-control login__yellow_inputs"
+                              placeholder="Lila4"
+                              name="lCuatro"
+                              value={ lCuatro }
+                              onChange={ handleRegisterInputChange }
+                          />
+                      </div>
+                      <div className="form-group col">
+                          <input
+                              type="number"
+                              className="form-control login__yellow_inputs"
+                              placeholder="Lila4"
+                              name="lCinco"
+                              value={ lCinco }
                               onChange={ handleRegisterInputChange }
                           />
                       </div>
